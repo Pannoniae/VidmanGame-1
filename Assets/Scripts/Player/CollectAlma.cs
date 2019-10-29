720ec245434929813dd320bc6d25175186c8b870
RealMarioD <mariocraft.MC2003@gmail.com>
2019-10-29 15:03:58 +0100

    Collectible Apples

---
+++ Assets/Scripts/Player/CollectAlma.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectAlma : MonoBehaviour
+{
+
+    public AudioSource CollectSound;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        CollectSound.Play();
+        ScoreSystem.Score += 10;
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Player/ScoreSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public GameObject ScoreText;
+    public static int Score;
+
+    void Update()
+    {
+        ScoreText.GetComponent<Text>().text = "SCORE: " + Score;
+    }
+}

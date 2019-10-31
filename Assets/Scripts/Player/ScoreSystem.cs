5f3b00e1dd32ce1f3cf9218bbe72b85b0338ecd0
RealMarioD <mariocraft.MC2003@gmail.com>
2019-10-31 12:23:27 +0100

    PREFABS
    Ha nem magyaráztam még el neked, hogy ez mire jó, kérj meg hogy mondjam el mert kurva fontos!

--- Assets/Scripts/Player/ScoreSystem.cs
+++ Assets/Scripts/Player/ScoreSystem.cs
@@ -5,17 +5,11 @@ using UnityEngine.UI;
 
 public class ScoreSystem : MonoBehaviour
 {
-    public GameObject ScoreText;
-    public static int Score;
-
-    void Start()
-    {
-        // ha savefile léteik és vagy continue volt megnyomva: loadscore else:
-        Score = 0;
-    }
+    public Text ScoreText;
+    public static int Score = 0;
 
     void Update()
     {
-        ScoreText.GetComponent<Text>().text = "SCORE: " + Score;
+        ScoreText.text = "SCORE: " + Score;
     }
 }

2f3033fac26cc021b46783b50e916e05e65e622d
Pannoniae <prisznyak.zsombor@gmail.com>
2019-11-05 22:14:57 +0000

    playercontroller fix

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,47 +1,25 @@
-﻿
-using UnityEngine;
-using UnityEngine.Events;
+﻿using UnityEngine;
 using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour {
-
     [FormerlySerializedAs("m_JumpForce")] [SerializeField]
-    private float jumpForce = 400f;
+    private float jumpForce = 500f;
 
     [FormerlySerializedAs("m_CrouchSpeed")] [Range(0, 1)] [SerializeField]
     private float crouchSpeed = .36f;
 
-    [FormerlySerializedAs("m_MovementSmoothing")] [Range(0, .3f)] [SerializeField]
-    private float movementSmoothing = .05f;
-
-    [FormerlySerializedAs("m_AirControl")] [SerializeField]
-    private bool airControl = false;
-
-    [FormerlySerializedAs("m_WhatIsGround")] [SerializeField]
-    private LayerMask whatIsGround;
-
-    [FormerlySerializedAs("m_GroundCheck")] [SerializeField]
-    private Transform groundCheck;
-
-    [FormerlySerializedAs("m_CeilingCheck")] [SerializeField]
-    private Transform ceilingCheck;
-
-    [FormerlySerializedAs("m_CrouchDisableCollider")] [SerializeField]
-    private Collider2D crouchDisableCollider;
-
-    bool isCrouchColliderPresent = false;
-
     private Rigidbody2D body;
 
-    const float groundedRadius = .2f;
     private bool grounded;
-    const float ceilingRadius = .2f;
     private bool facingRight = true;
-    private Vector3 velocity = Vector3.zero;
 
-    private Collider2D[] colliders = new Collider2D[100]; // increase if not enough
+    public float runSpeed = 40f;
 
-    private bool wasCrouching = false;
+    float horizontalMove;
+    bool jump;
+    bool crouch;
+
+    private bool wasCrouching;
 
     public Animator anim;
     private static readonly int IsWalking = Animator.StringToHash("isWalking");
@@ -51,62 +29,31 @@ public class PlayerMovement : MonoBehaviour {
     private void Awake() {
         anim = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
+    }
 
-        if (crouchDisableCollider != null) {
-            isCrouchColliderPresent = true;
+
+    public void Move(float move) {
+        if (crouch) {
+            if (!wasCrouching) {
+                wasCrouching = true;
+            }
+
+            move *= crouchSpeed;
         }
-    }
-    
-
-/*Physics2D.OverlapCircleNonAlloc(groundCheck.position, groundedRadius, colliders, whatIsGround);
-foreach (var t in colliders) {
-    if (t == null) continue;
-    if (t.gameObject != gameObject) {
-        grounded = true;
-        if (!wasGrounded)
-            OnLandEvent.Invoke();
-    }
-}*/
-
-
-    public void Move(float move, bool crouch, bool jump) {
-        if (!crouch) {
-            if (Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround)) {
-                crouch = true;
+        else {
+            if (wasCrouching) {
+                wasCrouching = false;
             }
         }
 
-        if (grounded || airControl) {
-            if (crouch) {
-                if (!wasCrouching) {
-                    wasCrouching = true;
-                }
+        body.velocity = new Vector2(move * 10, body.velocity.y);
 
-                move *= crouchSpeed;
 
-                if (isCrouchColliderPresent)
-                    crouchDisableCollider.enabled = false;
-            }
-            else {
-                if (isCrouchColliderPresent)
-                    crouchDisableCollider.enabled = true;
-
-                if (wasCrouching) {
-                    wasCrouching = false;
-                }
-            }
-
-            var velocity1 = body.velocity;
-            Vector3 targetVelocity = new Vector2(move * 10f, velocity1.y);
-            body.velocity = Vector3.SmoothDamp(velocity1, targetVelocity, ref velocity,
-                movementSmoothing);
-
-            if (move > 0 && !facingRight) {
-                Flip();
-            }
-            else if (move < 0 && facingRight) {
-                Flip();
-            }
+        if (move > 0 && !facingRight) {
+            Flip();
+        }
+        else if (move < 0 && facingRight) {
+            Flip();
         }
 
         if (grounded && jump) {
@@ -125,11 +72,9 @@ foreach (var t in colliders) {
         transform1.localScale = theScale;
     }
 
-    public float runSpeed = 40f;
-
-    float horizontalMove = 0f;
-    bool jump = false;
-    bool crouch = false;
+    public PlayerMovement() {
+        wasCrouching = false;
+    }
 
     void Update() {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
@@ -144,6 +89,7 @@ foreach (var t in colliders) {
         else if (Input.GetButtonUp("Crouch")) {
             crouch = false;
         }
+
         if (!Util.FloatEquals(Input.GetAxisRaw("Horizontal"), 0f)) {
             anim.SetBool(IsWalking, true);
         }
@@ -165,18 +111,25 @@ foreach (var t in colliders) {
     }
 
     void FixedUpdate() {
-        Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        Move(horizontalMove * Time.fixedDeltaTime);
         jump = false;
-        bool wasGrounded = grounded;
         grounded = false;
 
-        if (Util.FloatEquals(velocity.y, 0)) {
+        if (IsGrounded()) {
             grounded = true;
         }
 
-        if (!wasGrounded) {
-        }
+        print((body.velocity, grounded));
+    }
 
-        print((grounded, velocity));
+    public bool IsGrounded() {
+        var position = transform.position;
+        var direction = Vector2.down;
+        const float distance = 1.8f;
+
+        var hit = Physics2D.Raycast(position, direction, distance, (1 << 8));
+        Debug.DrawRay(position, direction * distance, Color.green);
+        print(hit.collider?.name);
+        return hit.collider != null;
     }
 }
